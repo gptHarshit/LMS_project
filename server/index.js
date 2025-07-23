d5130a4eb92fe6145b022a6aab8090fd0e4f6b87
Harsh <gupta.harshit0699@gmail.com>
2025-07-24 04:37:23 +0530

    Created course route and controller

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./database/db.js";
 import cors from "cors"
 import userRoute from "./routes/user.route.js"
+import courseRoute from "./routes/course.route.js"
 
 dotenv.config({});
 connectDB();
@@ -21,6 +22,8 @@ app.use(cors({
 }));
 
 app.use("/api/v1/user" , userRoute);
+app.use("/api/v1/course" , courseRoute);
+
 
 
 app.listen(PORT, () => {
