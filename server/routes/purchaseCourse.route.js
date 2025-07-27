c5be1f4bb1ab2429e93410ed1e7b7fac08896256
Harsh <gupta.harshit0699@gmail.com>
2025-07-27 19:10:54 +0530

    feat: Add static CourseProgress layout with video section and lecture list UI

--- server/routes/purchaseCourse.route.js
+++ server/routes/purchaseCourse.route.js
@@ -1,15 +1,17 @@
-import express from "express"
+import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { createCheckoutSession, stripeWebhook } from "../controllers/coursePurchase.controller.js";
+import {
+  createCheckoutSession,
+  getAllPurchasedCourse,
+  getCourseDetailWithPurchaseStatus,
+  stripeWebhook,
+} from "../controllers/coursePurchase.controller.js";
 
 const router = express.Router();
 
-router.route("/checkout/create-checkout-session").post(isAuthenticated,createCheckoutSession);
-router.route("/webhook").post(express.raw({type:"application/json"}),stripeWebhook);
-//router.route("/course/:courseId/detail-with-status").get();
-
-
-//router.route("/").get();
-
+router.route("/checkout/create-checkout-session").post(isAuthenticated, createCheckoutSession);
+router.route("/webhook").post(express.raw({ type: "application/json" }), stripeWebhook);
+router.route("/course/:courseId/detail-with-status").get( isAuthenticated,getCourseDetailWithPurchaseStatus);
+router.route("/").get( isAuthenticated,getAllPurchasedCourse);
 
 export default router;
